de8b287e7fff1de7c0e8c52fd492a7279f01c192
Alexander May <alexander.may@hjp-consulting.com>
2011-06-27 07:58:56 +0000

    add LabelProvider to add annotation to name of GtTestSpec projects (this behaviour is just for demonstration and should change to unique icon later)
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testspecification.ui/trunk@157 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/testspecification/ui/navigator/LabelProvider.java
@@ -0,0 +1,55 @@
+package org.globaltester.testspecification.ui.navigator;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.swt.graphics.Image;
+import org.globaltester.testspecification.natures.GtTestSpecNature;
+
+public class LabelProvider implements ILabelProvider {
+
+	@Override
+	public void addListener(ILabelProviderListener listener) {
+		// no listeners handled yet, as no events are communicated to them
+	}
+
+	@Override
+	public void dispose() {
+		// nothing to be done here
+	}
+
+	@Override
+	public boolean isLabelProperty(Object element, String property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeListener(ILabelProviderListener listener) {
+		// no listeners handled yet, as no events are communicated to them
+	}
+
+	@Override
+	public Image getImage(Object element) {
+		// no images provided yet
+		return null;
+	}
+
+	@Override
+	public String getText(Object element) {
+		// append
+		if (IProject.class.isInstance(element)) {
+			IProject proj = (IProject) element;
+			try {
+				if (proj.hasNature(GtTestSpecNature.NATURE_ID)) {
+					return proj.getName() + " (GT TestSpec)";
+				}
+			} catch (CoreException e) {
+				// ignore to label this project
+			}
+		}
+		return null;
+	}
+
+}
