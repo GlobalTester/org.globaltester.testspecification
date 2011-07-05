a17d4187eda17cd2adb2aad16652bcf26bd0d44b
Alexander May <alexander.may@hjp-consulting.com>
2011-07-05 08:25:23 +0000

    modified GTScanner structure to allow combination of different scanners to be used
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testspecification.ui/trunk@160 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/testspecification/ui/editors/TestSpecEditorDocumentProvider.java
+++ src/org/globaltester/testspecification/ui/editors/TestSpecEditorDocumentProvider.java
@@ -5,6 +5,9 @@ import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.rules.FastPartitioner;
 import org.eclipse.ui.editors.text.FileDocumentProvider;
+import org.globaltester.core.ui.editors.GTRuleBasedPartitionScanner;
+import org.globaltester.core.ui.editors.GTRuleBasedPartitionScanner.TokenType;
+import org.globaltester.core.ui.editors.XMLScanner;
 
 /**
  * This class defines the DocumentProvider of TestSpecEditor
@@ -20,6 +23,7 @@ public class TestSpecEditorDocumentProvider extends FileDocumentProvider {
 		IDocument document = super.createDocument(element);
 		if (document != null) {
 			GTRuleBasedPartitionScanner scanner = new GTRuleBasedPartitionScanner();
+			XMLScanner.addAllPredicateRules(scanner, TokenType.CONTENT_TYPE);
 			IDocumentPartitioner partitioner =
 				new FastPartitioner(
 					scanner, scanner.getLegalContentTypes());
