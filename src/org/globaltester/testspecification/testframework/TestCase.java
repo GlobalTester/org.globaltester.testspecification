ba0fe49d95977c7782a2f3da4d533a319361b277
Alexander May <alexander.may@hjp-consulting.com>
2011-07-11 10:25:31 +0000

    execute test steps from test spec
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testspecification/trunk@194 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/testspecification/testframework/TestCase.java
+++ src/org/globaltester/testspecification/testframework/TestCase.java
@@ -8,7 +8,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
 import org.globaltester.core.xml.XMLHelper;
-import org.jdom.Attribute;
+import org.globaltester.logging.logger.TestLogger;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.Namespace;
@@ -24,36 +24,40 @@ public class TestCase extends TestExecutable {
 	public TestCase(IFile iFile) throws CoreException {
 		super(iFile);
 	}
-	
+
 	@Override
 	protected void initFromIFile() {
 		Assert.isNotNull(iFile);
-		Document doc = XMLHelper.readDocument(iFile, false); //TODO enable validation here
+		Document doc = XMLHelper.readDocument(iFile, false); // TODO enable
+																// validation
+																// here
 		Element root = doc.getRootElement();
 		Namespace ns = root.getNamespace();
 
 		// check that root element has correct name
-		Assert.isTrue(root.getName().equals("TestCase"),"Root element is not TestCase");
-		
-		//extract metadata
+		Assert.isTrue(root.getName().equals("TestCase"),
+				"Root element is not TestCase");
+
+		// extract metadata
 		testCaseId = root.getAttribute("id").toString().trim();
 		testCaseTitle = root.getChild("Title", ns).getTextTrim();
 		testCaseVersion = root.getChild("Version", ns).getTextTrim();
 		testCasePurpose = root.getChild("Purpose", ns).getTextTrim();
-		
-		//extract TestSteps
+
+		// extract TestSteps
 		testSteps = new LinkedList<TestStep>();
 		@SuppressWarnings("unchecked")
-		List<Element> testStepElements =(List<Element>) root.getChildren("TestStep", ns);
+		List<Element> testStepElements = (List<Element>) root.getChildren(
+				"TestStep", ns);
 		for (Iterator<Element> iterator = testStepElements.iterator(); iterator
 				.hasNext();) {
-			Element curElem =  iterator.next();
+			Element curElem = iterator.next();
 			TestStep curTestStep = new TestStep(curElem);
 			if (curTestStep != null) {
 				testSteps.add(curTestStep);
 			}
 		}
-		
+
 	}
 
 	/**
@@ -63,19 +67,27 @@ public class TestCase extends TestExecutable {
 	 * @return
 	 */
 	public static boolean isFileRepresentation(IFile iFile) {
-		Document doc = XMLHelper.readDocument(iFile, false); //TODO enable validation here
+		Document doc = XMLHelper.readDocument(iFile, false); // TODO enable
+																// validation
+																// here
 		Element rootElem = doc.getRootElement();
 
 		// check that root element has correct name
 		if (!rootElem.getName().equals("TestCase")) {
 			return false;
 		}
-		
+
 		return true;
 	}
 
 	public List<TestStep> getTestSteps() {
 		return testSteps;
+	}
+
+	public void dumpTestCaseInformation() {
+		//TODO beautify this output
+		TestLogger.info("TestCaseId: "+testCaseId);
+		TestLogger.info("TestCaseTitle: "+testCaseTitle);
 	}
 
 }
--- src/org/globaltester/testspecification/testframework/TestStep.java
+++ src/org/globaltester/testspecification/testframework/TestStep.java
@@ -6,16 +6,20 @@ import org.jdom.Namespace;
 
 public class TestStep {
 
-
 	private Element command;
 	private String techCommand;
 
 	public TestStep(Element elem) {
-		Assert.isTrue(elem.getName().equals("TestStep"), "Element does not describe TestStep");
+		Assert.isTrue(elem.getName().equals("TestStep"),
+				"Element does not describe TestStep");
 		Namespace ns = elem.getNamespace();
-		
+
 		command = elem.getChild("Command", ns);
-		techCommand = elem.getChild("TechnicalCommand",ns).getTextTrim();
+		techCommand = elem.getChild("TechnicalCommand", ns).getTextTrim();
+	}
+
+	public String getTechnicalCommand() {
+		return techCommand;
 	}
 
 }
