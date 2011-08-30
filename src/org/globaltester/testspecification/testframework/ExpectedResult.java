73651244caf1e125ec622faa6d51b4eed7589e8f
Alexander May <alexander.may@hjp-consulting.com>
2011-08-30 07:41:19 +0000

    execute ExpectedResults
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testspecification/trunk@211 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/testspecification/testframework/ExpectedResult.java
@@ -0,0 +1,58 @@
+package org.globaltester.testspecification.testframework;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.runtime.Assert;
+import org.jdom.Element;
+import org.jdom.Namespace;
+
+public class ExpectedResult implements ITestExecutable{
+
+	private String resultId;
+	private String techResult;
+	private List<String> descriptions;
+	
+
+	public ExpectedResult(Element elem, String id) {
+		Assert.isTrue(elem.getName().equals("ExpectedResult"),
+				"Element does not describe ExpectedResult");
+		Namespace ns = elem.getNamespace();
+		
+		resultId = id;
+		
+		//extract TechincalResult if present
+		Element techResElem = elem.getChild("TechnicalResult", ns);
+		if (techResElem != null) {
+			techResult = techResElem.getTextTrim();
+		}
+		
+		//extract description elements
+		@SuppressWarnings("rawtypes")
+		Iterator descriptionElemsIter = elem.getChildren("Description", ns).iterator();
+		descriptions = new ArrayList<String>();
+		while (descriptionElemsIter.hasNext()) {
+			Object curElem = descriptionElemsIter.next();
+			if (curElem instanceof Element) {
+				
+				descriptions.add(((Element) curElem).getTextTrim());
+			}
+			
+		}
+		
+	}
+
+	public String getTechnicalResult() {
+		return techResult;
+	}
+
+	public String getId() {
+		return resultId;
+	}
+
+	public List<String> getDescriptions() {
+		return descriptions;
+	}
+
+}
--- src/org/globaltester/testspecification/testframework/TestStep.java
+++ src/org/globaltester/testspecification/testframework/TestStep.java
@@ -2,6 +2,7 @@ package org.globaltester.testspecification.testframework;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.core.runtime.Assert;
@@ -14,6 +15,7 @@ public class TestStep implements ITestExecutable{
 	private Element command;
 	private String techCommand;
 	private List<String> descriptions;
+	private LinkedList<ExpectedResult> expResults;
 	
 
 	public TestStep(Element elem, String id) {
@@ -37,8 +39,27 @@ public class TestStep implements ITestExecutable{
 			}
 			
 		}
+		
+		// extract ExpectedResults
+		expResults = new LinkedList<ExpectedResult>();
+		@SuppressWarnings("unchecked")
+		List<Element> testStepElements = (List<Element>) elem.getChildren(
+				"ExpectedResult", ns);
+		int resultId = 1;
+		for (Iterator<Element> iterator = testStepElements.iterator(); iterator
+				.hasNext();) {
+			Element curElem = iterator.next();
+			ExpectedResult curExpResult = new ExpectedResult(curElem, stepId + " - "+resultId++);
+			if (curExpResult != null) {
+				expResults.add(curExpResult);
+			}
+		}
 	}
 
+	public Element getCommand() {
+		return command;
+	}
+	
 	public String getTechnicalCommand() {
 		return techCommand;
 	}
@@ -49,6 +70,10 @@ public class TestStep implements ITestExecutable{
 
 	public List<String> getDescriptions() {
 		return descriptions;
+	}
+
+	public List<ExpectedResult> getExpectedResults() {
+		return expResults;
 	}
 
 }
