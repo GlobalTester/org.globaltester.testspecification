400e39ce5f73bed20b1132fe6018be70d31524ae
Lothar Bornemann <lothar.bornemann@hjp-consulting.com>
2012-01-23 09:23:25 +0000

    first version of GT2 scripts converter
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testspecification/trunk@357 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/testspecification/importer/GT2TestProjectImporter.java
@@ -0,0 +1,14 @@
+package org.globaltester.testspecification.importer;
+
+
+
+public class GT2TestProjectImporter {
+	
+	public static void importProject(Object object) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+
+}
