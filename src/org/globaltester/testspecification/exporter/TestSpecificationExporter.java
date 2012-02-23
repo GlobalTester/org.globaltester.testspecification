70b25becd84c03d792dc1814ee2badaea2bb00f9
Holger Funke <holger.funke@hjp-consulting.com>
2012-02-23 12:40:38 +0000

    Added exportable test specifications to export them as Open Office document
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testspecification/trunk@685 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/testspecification/exporter/TestSpecificationExporter.java
@@ -0,0 +1,8 @@
+package org.globaltester.testspecification.exporter;
+
+public class TestSpecificationExporter {
+
+	public static void exportTestSpecification(Object object) {
+		// TODO Auto-generated method stub
+	}
+}
