a1949ff609a6e9d9b7550a5ca9da45a1734b1334
Alexander May <alexander.may@hjp-consulting.com>
2012-03-13 07:36:24 +0000

    refactored Resource operations to GtResourceHelper
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testspecification/trunk@802 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/testspecification/GtTestSpecProject.java
+++ src/org/globaltester/testspecification/GtTestSpecProject.java
@@ -2,15 +2,12 @@ package org.globaltester.testspecification;
 
 import java.net.URI;
 
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
+import org.globaltester.core.resources.GtResourceHelper;
 
 
 public class GtTestSpecProject {
@@ -30,13 +27,13 @@ public class GtTestSpecProject {
 		Assert.isNotNull(projectName);
 		Assert.isTrue(projectName.trim().length() > 0);
 
-		IProject project = createEmptyProject(projectName, location);
+		IProject project = GtResourceHelper.createEmptyProject(projectName, location);
 		try {
-			addGtTestSpecNature(project);
+			GtResourceHelper.addNature(project, GtTestSpecNature.NATURE_ID);
 
 			String[] paths = { "TestData/Certificates", "TestData/Subroutines",
 					"TestCases" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-			addToProjectStructure(project, paths);
+			GtResourceHelper.addToProjectStructure(project, paths);
 		} catch (CoreException e) {
 			e.printStackTrace();
 			project = null;
@@ -53,93 +50,6 @@ public class GtTestSpecProject {
 		}
 
 		return project;
-	}
-
-	/**
-	 * Create an empty project
-	 * 
-	 * @param projectName
-	 *            name of the project to be created
-	 * @param location
-	 *            location where the project shall be created. If empty the
-	 *            default workspace location will be used.
-	 * 
-	 */
-	private static IProject createEmptyProject(String projectName, URI location) {
-		IProject newProject = ResourcesPlugin.getWorkspace().getRoot()
-				.getProject(projectName);
-
-		if (!newProject.exists()) {
-			URI projectLocation = location;
-			IProjectDescription desc = newProject.getWorkspace()
-					.newProjectDescription(newProject.getName());
-			if (location != null
-					&& ResourcesPlugin.getWorkspace().getRoot()
-							.getLocationURI().equals(location)) {
-				projectLocation = null;
-			}
-
-			desc.setLocationURI(projectLocation);
-			try {
-				newProject.create(desc, null);
-				if (!newProject.isOpen()) {
-					newProject.open(null);
-				}
-			} catch (CoreException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return newProject;
-	}
-
-	private static void createFolder(IFolder folder) throws CoreException {
-		IContainer parent = folder.getParent();
-		if (parent instanceof IFolder) {
-			createFolder((IFolder) parent);
-		}
-		if (!folder.exists()) {
-			folder.create(false, true, null);
-		}
-	}
-
-	/**
-	 * Create a folder structure from given paths.
-	 * 
-	 * @param project
-	 *            project to create the folders inside
-	 * @param paths
-	 *            array of relative paths of the folders to be created
-	 * @throws CoreException
-	 */
-	private static void addToProjectStructure(IProject project, String[] paths)
-			throws CoreException {
-		for (String currentPath : paths) {
-			IFolder currentFolder = project.getFolder(currentPath);
-			createFolder(currentFolder);
-		}
-	}
-
-	/**
-	 * Add the GtTestSpecNature to the given project.
-	 * 
-	 * @param project
-	 *            project to add the nature to
-	 * @throws CoreException
-	 */
-	private static void addGtTestSpecNature(IProject project)
-			throws CoreException {
-		if (!project.hasNature(GtTestSpecNature.NATURE_ID)) {
-			IProjectDescription description = project.getDescription();
-			String[] prevNatures = description.getNatureIds();
-			String[] newNatures = new String[prevNatures.length + 1];
-			System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
-			newNatures[prevNatures.length] = GtTestSpecNature.NATURE_ID;
-			description.setNatureIds(newNatures);
-
-			IProgressMonitor monitor = null;
-			project.setDescription(description, monitor);
-		}
 	}
 
 }
