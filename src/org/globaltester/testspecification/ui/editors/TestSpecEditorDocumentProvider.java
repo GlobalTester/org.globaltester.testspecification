75dc3d6cf64b56bc5573b2a464d6aee6ac078ede
Alexander May <alexander.may@hjp-consulting.com>
2012-05-20 20:50:46 +0000

    change super class to TextFileDocumentProvider in order to ask user for confirmation when deleting files which are opened in dirty editors
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testspecification.ui/trunk@1018 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/testspecification/ui/editors/TestSpecEditorDocumentProvider.java
+++ src/org/globaltester/testspecification/ui/editors/TestSpecEditorDocumentProvider.java
@@ -4,7 +4,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.rules.FastPartitioner;
-import org.eclipse.ui.editors.text.FileDocumentProvider;
+import org.eclipse.ui.editors.text.TextFileDocumentProvider;
 import org.globaltester.core.ui.editors.GtScanner;
 import org.globaltester.core.ui.editors.GtScanner.TokenType;
 import org.globaltester.core.ui.editors.JSScanner;
@@ -18,13 +18,13 @@ import org.globaltester.core.ui.editors.XMLScanner;
  */
 
 
-public class TestSpecEditorDocumentProvider extends FileDocumentProvider {
+public class TestSpecEditorDocumentProvider extends TextFileDocumentProvider {
 
 	
 	private GtScanner partitionScanner;
 
 	protected IDocument createDocument(Object element) throws CoreException {
-		IDocument document = super.createDocument(element);
+		IDocument document = super.getDocument(element);
 		if (document != null) {
 			GtScanner scanner = getPartitionScanner();
 			IDocumentPartitioner partitioner =
