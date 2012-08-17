9654c2c7c7cead65b4e8ba5fb807bc67ab4512be
Martin Boonk <martin.boonk@hjp-consulting.com>
2012-08-17 09:16:14 +0000

    import screenshots test modified to use uihelpers, possibility to take screenshots of a specific "uihelper"
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.testspecification.ui.test/trunk@1186 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/testspecification/ui/test/importOfATestSpecification_screenshotsTest.java
+++ src/org/globaltester/testspecification/ui/test/importOfATestSpecification_screenshotsTest.java
@@ -1,60 +1,23 @@
 package org.globaltester.testspecification.ui.test;
 
-import static org.junit.Assert.assertNotNull;
-
-import java.io.File;
-
-import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
-import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
-import org.eclipse.swtbot.swt.finder.waits.Conditions;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.core.runtime.CoreException;
+import org.globaltester.swtbot.uihelper.GlobalTesterUiHelper;
+import org.globaltester.swtbot.uihelper.ImportWizardUiHelper;
+import org.globaltester.swtbot.uihelper.NavigatorViewUiHelper;
 import org.junit.Test;
 
 public class importOfATestSpecification_screenshotsTest {
 
-	public static String gtMainWindowTitle = "GlobalTester";
-	private SWTWorkbenchBot bot = new SWTWorkbenchBot();
 	
 	@Test
-	public void testImportOfATestSpecification_screenshots(){
+	public void testImportOfATestSpecification_screenshots() throws CoreException{
 		
-		bot.waitUntil(Conditions.shellIsActive(gtMainWindowTitle));
-		
-	
-		SWTBotView activeView = bot.activeView();
-		
-		//close the welcome page, if it is shown
-		if ("Welcome".equals(activeView.getTitle())) {
-			bot.viewByTitle("Welcome").close();
-		}
-		
-		//get the GlobalTester View widget
-		SWTBotView globalTesterNavigatorView = bot.viewByTitle("GlobalTester Navigator");
-		bot.captureScreenshot("screenshots"+File.separator+"importTestSpec01.png");
+		GlobalTesterUiHelper.init();
+		NavigatorViewUiHelper navigator = GlobalTesterUiHelper.focusNavigatorView();
+		navigator.captureScreenshot("importTestSpec01.png");
+		ImportWizardUiHelper importWizard = navigator.openImportWizardByNavigatorContextMenu();
+		importWizard.captureScreenshot("importTestSpec02.png");
 
-		//get the tree view widget of the GT View
-		SWTBotTree navigatorTree = globalTesterNavigatorView.bot().tree();
-		
-		//Select Import
-		SWTBotMenu contextMenu = navigatorTree.contextMenu("Import...");
-		contextMenu.click();
-
-		
-		SWTBotShell importDialogBot = bot.shell("Import");
-		SWTBotTree importViewTree = importDialogBot.bot().tree();
-		
-		importViewTree.expandNode("GlobalTester", true);
-
-//		bot.button("Next >").click();
-		
-//		globalTesterItem.getNode("Import TestSpecification from Plugin").select();
-
-		bot.captureScreenshot("screenshots"+File.separator+"importTestSpec02.png");
-		
-		bot.button("Cancel").click();
-		assertNotNull(1);
 	}
 	
 }
