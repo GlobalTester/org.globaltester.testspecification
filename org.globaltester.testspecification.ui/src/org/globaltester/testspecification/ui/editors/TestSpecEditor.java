a88354dc9415d973c28537abd6ee36792e8fb6cf
Martin Boonk <martin.boonk@hjp-consulting.com>
2016-03-24 15:05:31 +0100

    Removes obsolete code

--- org.globaltester.testspecification.ui/src/org/globaltester/testspecification/ui/editors/TestSpecEditor.java
+++ org.globaltester.testspecification.ui/src/org/globaltester/testspecification/ui/editors/TestSpecEditor.java
@@ -1,14 +1,11 @@
 package org.globaltester.testspecification.ui.editors;
 
 import org.eclipse.core.filebuffers.FileBuffers;
-import org.eclipse.core.filebuffers.ITextFileBuffer;
 import org.eclipse.core.filebuffers.ITextFileBufferManager;
 import org.eclipse.core.filebuffers.LocationKind;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IPropertyListener;
-import org.eclipse.ui.IWorkbenchPartConstants;
 import org.eclipse.ui.part.FileEditorInput;
 import org.globaltester.base.ui.editors.FoldingEditor;
 import org.globaltester.logging.logger.GtErrorLogger;
@@ -20,12 +17,11 @@ import org.globaltester.testspecification.ui.Activator;
  * @author Alexander May
  * 
  */
-public class TestSpecEditor extends FoldingEditor implements IPropertyListener {
+public class TestSpecEditor extends FoldingEditor{
 
 	public TestSpecEditor() {
 		super();
 		setDocumentProvider(new TestSpecEditorDocumentProvider());
-		addPropertyListener(this);
 	}
 	
 	@Override
@@ -60,34 +56,6 @@ public class TestSpecEditor extends FoldingEditor implements IPropertyListener {
 	protected void doSetInput(IEditorInput input) throws CoreException {
 		super.doSetInput(input);
 		connect();
-	}
-
-	@Override
-	public void propertyChanged(Object source, int propId) {
-		if (this != source)
-			return;
-		if (propId == IWorkbenchPartConstants.PROP_DIRTY) {
-			setDirty(this.isDirty());
-		}
-
-	}
-
-	/**
-	 * Set the dirtyState for the IFile associated with this EditorInput based
-	 * on FileBuffers. This is required in order to prevent accidental deletion
-	 * when unsaved changes exist in Editors.
-	 * 
-	 * @param isDirty
-	 *            new dirty state
-	 */
-	public void setDirty(boolean isDirty) {
-		ITextFileBuffer buffer = null;
-		buffer = FileBuffers.getTextFileBufferManager().getTextFileBuffer(
-				((FileEditorInput) getEditorInput()).getFile().getFullPath(),
-				LocationKind.IFILE);
-		if (buffer != null) {
-			buffer.setDirty(isDirty);
-		}
 	}
 
 	@Override
