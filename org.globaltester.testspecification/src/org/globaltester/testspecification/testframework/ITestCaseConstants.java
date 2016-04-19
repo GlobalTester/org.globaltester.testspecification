c0e93c63cc31e14cd0adf0d038f43e558f4ffd8b
Julian Koch <julian.koch@hjp-consulting.com>
2016-04-19 14:44:44 +0200

    Add constants for xml tags to avoid directly use of hard coded Strings

---
+++ org.globaltester.testspecification/src/org/globaltester/testspecification/testframework/ITestCaseConstants.java
@@ -0,0 +1,24 @@
+package org.globaltester.testspecification.testframework;
+
+public interface ITestCaseConstants {
+	public static final String testCase = "TestCase";
+	public static final String id = "id";
+	public static final String title = "Title";
+	public static final String version = "Version";
+	public static final String purpose = "Purpose";
+	public static final String profile = "Profile";
+	public static final String reference = "Reference";
+	public static final String precondition = "Precondition";
+	public static final String teststep = "TestStep";
+	public static final String postcondition = "Postcondition";
+	public static final String command = "Command";
+	public static final String techCommand = "TechnicalCommand";
+	public static final String techResult = "TechnicalResult";
+	
+	//TestUnit
+	public static final String testUnit = "TestUnit";
+	
+	//TestLayer
+	public static final String testLayer = "TestLayer";
+	
+}
