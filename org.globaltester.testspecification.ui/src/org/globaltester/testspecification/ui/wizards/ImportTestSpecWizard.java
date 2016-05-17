8c945d3428a712023852f6fb85bfeea7e6052f71
Christian Stroh <christian.stroh@hjp-consulting.com>
2016-05-17 14:02:44 +0200

    Check if at least one test specification was selected in import wizard

--- org.globaltester.testspecification.ui/src/org/globaltester/testspecification/ui/wizards/ImportTestSpecWizard.java
+++ org.globaltester.testspecification.ui/src/org/globaltester/testspecification/ui/wizards/ImportTestSpecWizard.java
@@ -26,6 +26,7 @@ import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IImportWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
@@ -57,101 +58,113 @@ public class ImportTestSpecWizard extends Wizard implements IImportWizard {
 	public boolean performFinish() {
 		selectedBundles = _pageOne.getSelectedElements();
 		
-		IRunnableWithProgress importRunnable = new IRunnableWithProgress() {
-			
-			@Override
-			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-				monitor.beginTask("Importing testscript projects.", selectedBundles.size()*10);
+		if(selectedBundles.size() > 0) {
+			IRunnableWithProgress importRunnable = new IRunnableWithProgress() {
 				
-				boolean errors = false;
-				
-				for(IConfigurationElement currentlySelectedBundle : selectedBundles) {
-					String currentBundleName = currentlySelectedBundle.getAttribute(BUNDLE_NAME);
-					String currentBundleSymbolicName = currentlySelectedBundle.getAttribute(BUNDLE_SYMBOLIC_NAME);
+				@Override
+				public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+					monitor.beginTask("Importing testscript projects.", selectedBundles.size()*10);
 					
-					IWorkspace workspace = ResourcesPlugin.getWorkspace();
-					IProject conflictingProject = workspace.getRoot().getProject(currentBundleName);
+					boolean errors = false;
 					
-					if(conflictingProject.exists()) {
+					for(IConfigurationElement currentlySelectedBundle : selectedBundles) {
+						String currentBundleName = currentlySelectedBundle.getAttribute(BUNDLE_NAME);
+						String currentBundleSymbolicName = currentlySelectedBundle.getAttribute(BUNDLE_SYMBOLIC_NAME);
+						
+						IWorkspace workspace = ResourcesPlugin.getWorkspace();
+						IProject conflictingProject = workspace.getRoot().getProject(currentBundleName);
+						
+						if(conflictingProject.exists()) {
+							
+							try {
+								conflictingProject.delete(true, true, null);
+								workspace.getRoot().refreshLocal(IResource.DEPTH_INFINITE, null);
+							} catch (CoreException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							
+						}
+						
+						monitor.subTask("Importing project " + currentBundleName + ".");
+						
+						monitor.subTask("Creating project " + currentBundleName + ".");
+						IProject project = GtTestSpecProject.createProject(currentBundleName, null);
+						monitor.worked(1);
+						
+						monitor.subTask("Copying content of bundle " + currentBundleSymbolicName + ".");
+						
+						Bundle curBundle = Platform.getBundle(currentBundleSymbolicName);
+						Enumeration<URL> allFiles = curBundle.findEntries("/", "*", false);
+						ArrayList<String> files = new ArrayList<>();
+						while(allFiles.hasMoreElements()) {
+							files.add(allFiles.nextElement().getFile());
+						}
+						String[] filesStrings = files.toArray(new String[1]);
 						
 						try {
-							conflictingProject.delete(true, true, null);
-							workspace.getRoot().refreshLocal(IResource.DEPTH_INFINITE, null);
+							GtResourceHelper.copyPluginFilesToWorkspaceProject(currentBundleSymbolicName, project, "/", filesStrings);
+						} catch (IOException e) {
+							errors = true;
+							GtErrorLogger.log(Activator.PLUGIN_ID, e);
+							Status st = new Status(IStatus.WARNING, Activator.PLUGIN_ID, e.getLocalizedMessage(), e);
+							ErrorDialog.openError(getShell(), "Unable to copy project " + currentBundleName + " to workspace.", "The selected project " + currentBundleName + " could not be imported into the workspace.", st);
+						} finally {
+							monitor.worked(8);
+						}
+						
+						monitor.subTask("Refreshing workspace.");
+						// refresh the workspace
+						try {
+							ResourcesPlugin.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_INFINITE, null);
 						} catch (CoreException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+							// refresh workspace failed
+							// log CoreException to eclipse log
+							GtErrorLogger.log(Activator.PLUGIN_ID, e);
+							
+							// users most probably will ignore this behavior and refresh manually, so do not open annoying dialog
+						} finally{
+							monitor.worked(1);
 						}
 						
 					}
 					
-					monitor.subTask("Importing project " + currentBundleName + ".");
-					
-					monitor.subTask("Creating project " + currentBundleName + ".");
-					IProject project = GtTestSpecProject.createProject(currentBundleName, null);
-					monitor.worked(1);
-					
-					monitor.subTask("Copying content of bundle " + currentBundleSymbolicName + ".");
-					
-					Bundle curBundle = Platform.getBundle(currentBundleSymbolicName);
-					Enumeration<URL> allFiles = curBundle.findEntries("/", "*", false);
-					ArrayList<String> files = new ArrayList<>();
-					while(allFiles.hasMoreElements()) {
-						files.add(allFiles.nextElement().getFile());
-					}
-					String[] filesStrings = files.toArray(new String[1]);
-					
-					try {
-						GtResourceHelper.copyPluginFilesToWorkspaceProject(currentBundleSymbolicName, project, "/", filesStrings);
-					} catch (IOException e) {
-						errors = true;
-						GtErrorLogger.log(Activator.PLUGIN_ID, e);
-						Status st = new Status(IStatus.WARNING, Activator.PLUGIN_ID, e.getLocalizedMessage(), e);
-						ErrorDialog.openError(getShell(), "Unable to copy project " + currentBundleName + " to workspace.", "The selected project " + currentBundleName + " could not be imported into the workspace.", st);
-					} finally {
-						monitor.worked(8);
+					if (!errors){
+						PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+						public void run() {
+						MessageDialog.openInformation(getShell(), "Import", "Testspecification(s) imported successfully.");
+						}}); 
 					}
 					
-					monitor.subTask("Refreshing workspace.");
-					// refresh the workspace
-					try {
-						ResourcesPlugin.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_INFINITE, null);
-					} catch (CoreException e) {
-						// refresh workspace failed
-						// log CoreException to eclipse log
-						GtErrorLogger.log(Activator.PLUGIN_ID, e);
-						
-						// users most probably will ignore this behavior and refresh manually, so do not open annoying dialog
-					} finally{
-						monitor.worked(1);
-					}
-					
+					monitor.done();
 				}
-				
-				if (!errors){
-					PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
-					public void run() {
-					MessageDialog.openInformation(getShell(), "Import", "Testspecification(s) imported successfully.");
-					}}); 
-				}
-				
-				monitor.done();
+			
+			};
+			
+			ProgressMonitorDialog pmd = new ProgressMonitorDialog(getShell());
+			pmd.setOpenOnRun(true);
+			pmd.setBlockOnOpen(false);
+			
+			try {
+				pmd.run(true, true, importRunnable);
+			} catch (InvocationTargetException e) {
+				GtErrorLogger.log(Activator.PLUGIN_ID, e);
+			} catch (InterruptedException e) {
+				GtErrorLogger.log(Activator.PLUGIN_ID, e);
 			}
-		
-		};
-		
-		ProgressMonitorDialog pmd = new ProgressMonitorDialog(getShell());
-		pmd.setOpenOnRun(true);
-		pmd.setBlockOnOpen(false);
-		
-		try {
-			pmd.run(true, true, importRunnable);
-		} catch (InvocationTargetException e) {
-			GtErrorLogger.log(Activator.PLUGIN_ID, e);
-		} catch (InterruptedException e) {
-			GtErrorLogger.log(Activator.PLUGIN_ID, e);
+			
+			return true;
+		} else {
+			Display.getDefault().asyncExec(new Runnable() {
+				public void run() {
+					MessageDialog.openWarning(null, "Warning",
+							"No TestSpecification project was selected. Please select at least one project or click Cancel to leave the wizard page.");
+				}
+			});
+			return false;
 		}
 		
-		return true;
+		
 	}
 
 	@Override
